5b05095ee236dc3101d45397aa7f6b76d0669f9f
nikhilbhide <nikhilbhide15@gmail.com>
2020-06-05 21:00:20 +0530

    added main command file the application

--- cmd/imageApp.go
+++ cmd/imageApp.go
@@ -1,25 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/nik/platform-image-service/pkg/domain/repository"
 	"github.com/nik/platform-image-service/pkg/domain/service"
-	"github.com/nik/platform-image-service/pkg/infra"
+	"github.com/nik/platform-image-service/pkg/infra/cassandra"
+	"github.com/nik/platform-image-service/utility"
 )
 
 func main() {
-	//create cassandra connection instance
-	conn := &cassandra.CassandraConn{
-		Hosts:       []string{"172.18.0.2"},
-		Port:        "9042",
-		User:        "cassandra",
-		Password:    "cassandra",
-		Consistency: "Quorum",
-		Keyspace:    "platform_image_db",
-	}
 
+	//load configuration
+	config, err := utility.LoadConfiguration("/etc/config/config.json")
+	if err != nil {
+		//halt bootstrapping
+		fmt.Println("Error in loading configuration - ", err)
+	}
+	//instantiate cassandra connection instance
+	conn := &cassandra.CassandraConn{
+		Hosts:       []string{config.Cassandra.Host},
+		Port:        config.Cassandra.Port,
+		User:        config.Cassandra.User,
+		Password:    config.Cassandra.Password,
+		Consistency: config.Cassandra.Consistency,
+		Keyspace:    config.Cassandra.Keyspace,
+		CaPath:      config.Cassandra.SSLCertPath,
+	}
 	//create repoinstance
 	repoInstance := repository.NewCassandraAPIMetadataRepo(conn)
 	apiServiceInstance := service.NewAPIService(repoInstance)
+	//imageMetadataRepo:= repository.NewCassandraAPIMetadataRepo(conn)
 	imageSearch := service.NewImageService(apiServiceInstance)
-	imageSearch.Search("TEST01", "cars")
+	//imageSearch.Search("TEST01", "cars",0, 10)
+	imageSearch.Search("TEST01", "computers")
 }
