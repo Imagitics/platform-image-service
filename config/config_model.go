ca6d01550763edfb27bb06662e32c2bdbdfd8ee8
nikhilbhide <nikhilbhide15@gmail.com>
2020-06-05 21:00:59 +0530

    added configuration for application

---
+++ config/config_model.go
@@ -0,0 +1,19 @@
+package config
+
+type ConfigModel struct {
+	Google_API_METAMODEL struct {
+		AccessKey  string `json:"aws_access_key"`
+		SecretKey  string `json:"aws_secret_key"`
+		Region     string `json:"region"`
+		PathPrefix string `json:"path_prefix"`
+	} `json:"s3"`
+	Cassandra struct {
+		Host        string `json:"host"`
+		Port        string `json:"port"`
+		User        string `json:"user"`
+		Password    string `json:"password"`
+		SSLCertPath string `json:"cert_file_path"`
+		Consistency string `json:"consistency"`
+		Keyspace    string `json:"keyspace"`
+	}
+}
