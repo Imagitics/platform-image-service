15a81637e1c3099dca0c5ef4572600e787baecf9
nikhilbhide <nikhilbhide15@gmail.com>
2020-06-10 18:07:38 +0530

    added utility methods for file operations

--- utility/util.go
+++ utility/util.go
@@ -1,9 +1,14 @@
 package utility
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/nik/platform-image-service/config"
+	"io"
+	"mime/multipart"
+	"net/http"
 	"os"
+	"path/filepath"
 )
 
 // Loads the configuration from the properties  file
@@ -19,4 +24,55 @@ func LoadConfiguration(file string) (*config.ConfigModel, error) {
 	jsonParser.Decode(&config)
 
 	return &config, nil
+}
+
+//DownloadFile downloads a url to a local file
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+// Creates a new file upload http request with optional extra params
+func NewfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(part, file)
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", uri, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	return req, err
 }
