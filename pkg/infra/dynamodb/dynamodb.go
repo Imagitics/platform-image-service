b7920df8ff94a81b32bbc246b5c99b80de8920e1
nikhilbhide <nikhilbhide15@gmail.com>
2020-08-09 15:44:36 +0530

    added dynamodb support

---
+++ pkg/infra/dynamodb/dynamodb.go
@@ -0,0 +1,30 @@
+package dynamo_db
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/nik/platform-image-service/config"
+	"github.com/nik/platform-image-service/logger"
+)
+
+type DynamoDBConn struct {
+	Endpoint string
+}
+
+//InitSession creates a session with dynamodb
+func (conn *DynamoDBConn) InitSession(awsConfigModel *config.AWS) *dynamodb.DynamoDB {
+	logger := logger.GetInstance()
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(awsConfigModel.Region),
+		Credentials: credentials.NewStaticCredentials(awsConfigModel.AccessKey, awsConfigModel.SecretKey, "TOKEN"),
+	})
+
+	if err != nil {
+		logger.Sugar().Infof("Instantiation of aws session is failed with %s", err.Error())
+	}
+	//instantiate the dynamodb service
+	svc := dynamodb.New(sess, &aws.Config{Endpoint: aws.String(conn.Endpoint)})
+	return svc
+}
